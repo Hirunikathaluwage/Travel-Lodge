2b691ed91ee37c1dbc039df5e76a1afd7e300dc4
Hiruni <kathaluwagehiruni@gmail.com>
2025-12-05 16:37:39 +0530

    Connect with the db

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const db = process.env.MongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log(" Mondodb Connected"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Server working ");
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {(`Server running on port port ${port} `);
+
+});
+
+
