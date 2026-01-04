00b10f1acd0cfddbed3b945cdff7e2e5a428e5fe
Hiruni <kathaluwagehiruni@gmail.com>
2026-01-04 14:49:45 +0530

    fixed register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,24 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.create({
+    username,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "User created successfully" });
+};
+
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -33,7 +51,6 @@ export const login = async (req, res) => {
     });
 
   } catch (error) {
-    console.error("Login error:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
